89c19524f25fd37b887eb8b8c90148e9f965a9f3
bigh0124 <thubgi0629@gmail.com>
2025-06-25 16:26:22 +0800

    feat: 新增 application metric handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -27,6 +28,9 @@ func (app *application) routes() http.Handler {
 
 	//tokens
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	//application metrics
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
